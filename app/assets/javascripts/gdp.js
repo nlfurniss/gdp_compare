b5ca00bcdd2ce9ac0cf8ea4bb9ccb795b0c08085
nlfurniss <nlfurniss@gmail.com>
2014-02-10 14:47:50 -0800

    searches modify URL for easy sharing, log scale deep-linkable, refactored controller

--- app/assets/javascripts/gdp.js
+++ app/assets/javascripts/gdp.js
@@ -1,5 +1,71 @@
 window.GDP = {
 
+    chartOptions: {
+        chart: {
+            renderTo: 'chart',
+            type: 'line',
+            //backgroundColor: '#F2F1ED',
+            borderColor: '#000000',
+            borderWidth: 1,
+            height: 500
+        },
+        title: {
+            text: null
+        },
+        series: window.dataToLoad,
+        xAxis: {
+            title: {
+                text: 'Year',
+                margin: 18
+            },
+            labels: {
+                y: 20
+            }
+        },
+        yAxis: {
+            title: {
+                text: 'GDP/capita 1990 USD',
+                margin: 15
+            },
+            type: 'linear',
+            labels: {
+                formatter: function() {return '$' + Highcharts.numberFormat(this.value,0);},
+                x: -6,
+                y: 4
+            },
+            min: 1
+        },
+        colors: [
+            '#FF851B',
+            '#001F3F'
+        ],
+        plotOptions: {
+            line: {
+                marker: {
+                    enabled: false
+                },
+                tooltip: {
+                    valuePrefix: '$'
+                }
+            }
+        },
+        legend: {
+            align: 'center',
+            verticalAlign: 'top',
+            y: 0,
+        },
+        tooltip: {
+            crosshairs: true
+        },
+        exporting: {
+            buttons: {
+                contextButton: {
+                    text: 'Export'
+                }
+            }
+        }
+    },
+
     initialize: function() {
         var self = this;
 
@@ -9,9 +75,16 @@ window.GDP = {
             self.fetchData(event);
         });
 
-        $('.logScale input').on('change', function(event) {
+        var $scale = $('.logScale input');
+
+        $scale.on('change', function(event) {
             self.toggelLogScale(event.target.checked);
         });
+
+        // If the deep-link has log scale, set that before the chart renders
+        if ($scale.is(':checked')) {
+            this.chartOptions.yAxis.type = 'logarithmic';
+        }
 
         // Add autocompletes to the inputs
         $('.countries').autocomplete({
@@ -19,74 +92,7 @@ window.GDP = {
         });
 
         // Setup the World GDP chart
-        this.chart = new Highcharts.Chart({
-            chart: {
-                renderTo: 'chart',
-                type: 'line',
-                //backgroundColor: '#F2F1ED',
-                borderColor: '#000000',
-                borderWidth: 1,
-                height: 500
-            },
-            title: {
-                text: null
-            },
-            series: [{
-                name: 'Total World',
-                data: window.worldGDP,
-            }],
-            xAxis: {
-                title: {
-                    text: 'Year',
-                    margin: 18
-                },
-                labels: {
-                    y: 20
-                }
-            },
-            yAxis: {
-                title: {
-                    text: 'GDP/capita 1990 USD',
-                    margin: 15
-                },
-                type: 'linear',
-                labels: {
-                    formatter: function() {return '$' + Highcharts.numberFormat(this.value,0);},
-                    x: -6,
-                    y: 4
-                },
-                min: 1
-            },
-            colors: [
-                '#FF851B',
-                '#001F3F'
-            ],
-            plotOptions: {
-                line: {
-                    marker: {
-                        enabled: false
-                    },
-                    tooltip: {
-                        valuePrefix: '$'
-                    }
-                }
-            },
-            legend: {
-                align: 'center',
-                verticalAlign: 'top',
-                y: 0,
-            },
-            tooltip: {
-                crosshairs: true
-            },
-            exporting: {
-                buttons: {
-                    contextButton: {
-                        text: 'Export'
-                    }
-                }
-            }
-        });
+        this.chart = new Highcharts.Chart(this.chartOptions);
     },
 
     fetchData: function(event) {
@@ -114,6 +120,16 @@ window.GDP = {
 
     success: function(data, status) {
         this.addData(data);
+        var title, queryParams;
+        if (data.length > 1) {
+            title = 'GDP Comparisons: ' + data[0].name + ' vs. ' + data[1].name;
+            queryParams = '?regions=' + data[0].name + ',' + data[1].name;
+        } else {
+            title = 'GDP Comparisons: ' + data[0].name;
+            queryParams = '?regions=' + data[0].name;
+        }
+        $('title')[0].innerHTML = title;
+        history.pushState({}, document.title, queryParams);
     },
 
     error: function(jqXHR, textStatus, error) {
@@ -138,12 +154,14 @@ window.GDP = {
         switch (value) {
             case true:
                 this.chart.yAxis[0].update({type: 'logarithmic'});
+                history.pushState({}, document.title, window.location.search + '&scale=log');
                 break;
             case false:
                 this.chart.yAxis[0].update({type: 'linear'});
+                history.pushState({}, document.title, window.location.search.replace('&scale=log', ''));
                 break;
         }
     }
 };
 
-$(function(){window.GDP.initialize()});
+$(function(){window.GDP.initialize();});
--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,31 +1,39 @@
 class PagesController < ApplicationController
-    before_action :return_regions
+    before_action :prep_regions
 
     def index
+        @title = 'GDP Comparison'
+        @countries = Region.pluck(:name)
+        @scale = params[:scale]
+
         if @regions
-            @regions.split(',').each do |region|
-                data = Region.find_by_name(region).incomes.where("gdp > 0").pluck(:year, :gdp)
-                @response.push({name: region, data: data})
-            end
+            query_data(@regions)
+            @title += ': ' + (@regions.length > 1 ? @regions[0] + ' vs. ' + @regions[1] : @regions[0])
         else
-            @countries = Region.pluck(:name)
-            @world_gdp = Region.find_by_name("Poland").incomes.where("gdp > 0").pluck(:year, :gdp)
+            @response = [{name: "Total World", data: Region.find_by_name("Total World").incomes.where("gdp > 0").pluck(:year, :gdp)}]
         end
+
+        @response = @response.to_json
     end
 
     def get_data
-        @response = []
-        @regions.gsub(/^,/, '').split(',').each do |region| #.gsub!(/^,/, '')
-            data = Region.find_by_name(region).incomes.where("gdp > 0").pluck(:year, :gdp)
-            @response.push({name: region, data: data})
-        end
+        query_data(@regions)
         render json: @response
     end
 
     private
 
-    def return_regions
-        @regions = params[:regions].blank? ? nil : params[:regions]
+    def prep_regions
+        @regions = params[:regions].blank? ? nil : params[:regions].gsub(/^,/, '').split(',')
+    end
+
+    def query_data(regions)
+        @response = []
+        @regions.each do |region|
+            data = Region.find_by_name(region.titlecase).try(:incomes).where("gdp > 0").pluck(:year, :gdp) rescue []
+            @response.push({name: region, data: data})
+        end
+        @response
     end
 
 end
